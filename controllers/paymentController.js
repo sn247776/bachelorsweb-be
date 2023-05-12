0b8e8a636796ede4bdc06e1fef46c4e6295371af
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-12 19:29:38 +0530

    work on payment interigaiton

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,7 +3,7 @@ import { User } from "../models/User.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { instance } from "../index.js";
 import crypto from "crypto";
-// import { Payment } from "../models/Payment.js";
+import { Payment } from "../models/Payment.js";
 
 export const buySubscription = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
@@ -30,3 +30,76 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
     subscriptionId: subscription.id,
   });
 });
+
+export const paymentVerification = catchAsyncError(async (req, res, next) => {
+  const { razorpay_signature, razorpay_payment_id, razorpay_subscription_id } =
+    req.body;
+
+  const user = await User.findById(req.user._id);
+
+  const subscribe_id = user.subscription.id;
+
+  const generated_signature = crypto
+    .createHmac("sha256", process.env.RAZOR_SECRET)
+    .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
+    .digest("hex");
+
+  const isAuthentic = generated_signature === razorpay_signature;
+
+  if (!isAuthentic) return res.redirect(`${process.env.FE_URL}/paymentfail`);
+
+  await Payment.create({
+    razorpay_signature,
+    razorpay_payment_id,
+    razorpay_subscription_id,
+  });
+  user.subscription.status = "active";
+
+  await user.save();
+
+  res.redirect(
+    `${process.env.FE_URL}/paymentsuccess?reference=${razorpay_payment_id}`
+  );
+});
+
+export const getRazorPayKey = catchAsyncError(async (req, res, next) => {
+    res.status(200).json({
+      success: true,
+      key: process.env.RAZOR_API_KEY,
+    });
+  });
+
+
+  export const cancelSubscription = catchAsyncError(async (req, res, next) => {
+    const user = await User.findById(req.user._id);
+  
+    const subscriptionId = user.subscription.id;
+    let refund = false;
+  
+    await instance.subscriptions.cancel(subscriptionId);
+  
+    const payment = await Payment.findOne({
+      razorpay_subscription_id: subscriptionId,
+    });
+  
+    const gap = Date.now() - payment.createdAt;
+  
+    const refundTime = process.env.REFUND_DAYS * 24 * 60 * 60 * 1000;
+  
+    if (refundTime > gap) {
+      await instance.payments.refund(payment.razorpay_payment_id);
+      refund = true;
+    }
+  
+    await payment.deleteOne();
+    user.subscription.id = undefined;
+    user.subscription.status = undefined;
+    await user.save();
+  
+    res.status(200).json({
+      success: true,
+      message: refund
+        ? "Subscription cancelled, You will receive full refund within 7 days."
+        : "Subscription cancelled, Now refund initiated as subscription was cancelled after 7 days.",
+    });
+  });
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
 import {isAuthenticated} from "../middlewares/Auth.js"
-import { buySubscription } from "../controllers/paymentController.js";
+import { buySubscription, cancelSubscription, getRazorPayKey, paymentVerification } from "../controllers/paymentController.js";
 
 
 const router = express.Router();
 
 router.route("/subscribe").get(isAuthenticated,buySubscription)
 
+router.route("/paymentverification").post(isAuthenticated,paymentVerification)
+
+router.route("/razorpaykey").get(getRazorPayKey);
+
+router.route("/subscribe/cancel").delete(isAuthenticated, cancelSubscription);
 
 export default router;
