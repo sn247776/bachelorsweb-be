5c72314e7338c8f3f2ae6a8f598be6f8cd1e0614
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-12 12:21:00 +0530

    New Routes are added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,32 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { User } from "../models/User.js";
-import {Course} from "../models/Course.js"
+import { Course } from "../models/Course.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { sendEmail } from "../utils/sendEmail.js";
 import { sendToken } from "../utils/sendToken.js";
 import crypto from "crypto";
+import cloudinary from "cloudinary";
+import getDataUri from "../utils/dataUri.js";
 
 export const register = catchAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
-  // const file = req.file;
-  if (!name || !email || !password)
+  const file = req.file;
+  if (!name || !email || !password || !file)
     return next(new ErrorHandler("Please enter all field", 400));
 
   let user = await User.findOne({ email });
   if (user) return next(new ErrorHandler("User Already Exist", 409));
 
-  // Upload file on cloudinary;
+  const fileUri = getDataUri(file);
+  const mycloud = await cloudinary.v2.uploader.upload(fileUri.content);
 
   user = await User.create({
     name,
     email,
     password,
     avatar: {
-      public_id: "tempid",
-      url: "tempurl",
+      public_id: mycloud.public_id,
+      url: mycloud.secure_url,
     },
   });
   sendToken(res, user, "Registered Successfully", 201);
@@ -92,6 +95,18 @@ export const updateProfile = catchAsyncError(async (req, res, next) => {
 });
 
 export const updateProfilePic = catchAsyncError(async (req, res, next) => {
+  const file = req.file;
+  const user = await User.findById(req.user._id);
+  const fileUri = getDataUri(file);
+  const mycloud = await cloudinary.v2.uploader.upload(fileUri.content);
+  await cloudinary.v2.uploader.destroy(user.avatar.public_id);
+
+  user.avatar = {
+    public_id: mycloud.public_id,
+    url: mycloud.secure_url,
+  };
+
+  await user.save();
   res.status(200).json({
     success: true,
     message: "Profile Picture Updated Successfully",
@@ -118,70 +133,128 @@ export const forgetPassword = catchAsyncError(async (req, res, next) => {
 });
 
 export const resetPassword = catchAsyncError(async (req, res, next) => {
+  const { token } = req.params;
 
-  const {token} = req.params;
-
-  const  resetPasswordToken = crypto
-  .createHash("sha256")
-  .update(token)
-  .digest("hex");
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
 
   const user = await User.findOne({
     resetPasswordToken,
-    resetPasswordExpire:{
-      $gt:Date.now(),
+    resetPasswordExpire: {
+      $gt: Date.now(),
     },
   });
 
-
-  if (!user) return next(new ErrorHandler("Token is invalid or has been expired",401));
+  if (!user)
+    return next(new ErrorHandler("Token is invalid or has been expired", 401));
 
   user.password = req.body.password;
   user.resetPasswordExpire = undefined;
   user.resetPasswordToken = undefined;
 
   await user.save();
-  
+
   res.status(200).json({
     success: true,
     message: "Password Changed Successfully",
   });
 });
 
-
-export const addtoPlaylist = catchAsyncError(async(req,res,next)=>{
+export const addtoPlaylist = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
-  const course = await Course.findById(req.body.id)
+  const course = await Course.findById(req.body.id);
 
-  if(!course) return next(new ErrorHandler("Invalid Course Id", 404));
-  const itemExist = user.playlist.find((item)=>{
-    if(item.course.toString() === course._id.toString())
-    return true
-  })
+  if (!course) return next(new ErrorHandler("Invalid Course Id", 404));
+  const itemExist = user.playlist.find((item) => {
+    if (item.course.toString() === course._id.toString()) return true;
+  });
 
-  if(itemExist) return next(new ErrorHandler("Item Already Exist", 409));
+  if (itemExist) return next(new ErrorHandler("Item Already Exist", 409));
   user.playlist.push({
     course: course._id,
     poster: course.poster.url,
   });
-  await user.save()
+  await user.save();
   res.status(200).json({
     success: true,
     message: "Playlist Added Successfully",
   });
-})
+});
 
-export const removefromPlaylist = catchAsyncError(async(req,res,next)=>{
+export const removefromPlaylist = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
-  const course = await Course.findById(req.query.id)
-  if(!course) return next(new ErrorHandler("Invalid Course Id", 404));
-  const newPlaylist = user.playlist.filter((item)=>{
+  const course = await Course.findById(req.query.id);
+  if (!course) return next(new ErrorHandler("Invalid Course Id", 404));
+  const newPlaylist = user.playlist.filter((item) => {
     if (item.course.toString() !== course._id.toString()) return item;
   });
   user.playlist = newPlaylist;
-  await user.save()
+  await user.save();
   res.status(200).json({
     success: true,
     message: "Removed From Playlist Successfully",
   });
-})
+});
+
+// Admin Controllers
+
+export const getAllUsers = catchAsyncError(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+export const changeUserRole = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  if (user.role === "user") user.role = "admin";
+  else user.role = "user";
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: `Role Updated in ${user.role}`,
+  });
+});
+
+export const deleteUser = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  await cloudinary.v2.uploader.destroy(user.avatar.public_id);
+
+  // Cancel Subscription
+
+  await user.deleteOne()
+
+  res.status(200).json({
+    success: true,
+    message: `${user.name} is Deleted`,
+  });
+});
+
+
+export const deleteMyProfile = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  await cloudinary.v2.uploader.destroy(user.avatar.public_id);
+
+  // Cancel Subscription
+
+  await user.deleteOne()
+
+  res.status(200).cookie("sendtoken", null,{expires: new Date(Date.now())}).json({
+    success: true,
+    message: `${user.name} is Deleted`,
+  });
+});
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,11 @@ import express from "express";
 import {
   addtoPlaylist,
   changePassword,
+  changeUserRole,
+  deleteMyProfile,
+  deleteUser,
   forgetPassword,
+  getAllUsers,
   getMyProfile,
   login,
   logout,
@@ -12,18 +16,24 @@ import {
   updateProfile,
   updateProfilePic,
 } from "../controllers/userController.js";
-import { isAuthenticated } from "../middlewares/Auth.js";
+import { authorizeAdmin, isAuthenticated } from "../middlewares/Auth.js";
+import singleUpload from "../middlewares/multer.js"
 
 const router = express.Router();
-router.route("/register").post(register);
+router.route("/register").post(singleUpload, register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticated, getMyProfile);
+router.route("/me").delete(isAuthenticated, deleteMyProfile);
 router.route("/changepassword").put(isAuthenticated, changePassword);
 router.route("/updateprofile").put(isAuthenticated, updateProfile);
-router.route("/updateprofilepic").put(isAuthenticated, updateProfilePic);
+router.route("/updateprofilepic").put(isAuthenticated,singleUpload, updateProfilePic);
 router.route("/forgetpassword").post(forgetPassword);
 router.route("/resetpassword/:token").put(resetPassword);
 router.route("/addtoplaylist").post(isAuthenticated, addtoPlaylist);
 router.route("/removefromplaylist").delete(isAuthenticated, removefromPlaylist);
+
+// Admin Routes
+router.route("/admin/users").get(isAuthenticated, authorizeAdmin, getAllUsers);
+router.route("/admin/user/:id").put(isAuthenticated, authorizeAdmin, changeUserRole).delete(isAuthenticated, authorizeAdmin, deleteUser)
 export default router;
